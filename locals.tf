1464ec296dcad9f8f3dae9f932f5d40828f3f969
murali <murali.pydi@gmail.com>
2025-12-03 19:07:01 +0530

    code change in files

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals{
-    ami_id = "ami-09c813fb71547fc4f" #data.aws_ami.joindevops.AMI_ID  #"ami-09c813fb71547fc4f" #data.aws_ami.joindevops.id
+    ami_id = "ami-09c813fb71547fc4f" #data.aws_ami.joindevops.id  # #data.aws_ami.joindevops.id
     common_tags = {
         Project = var.project
         Environment = var.environment
