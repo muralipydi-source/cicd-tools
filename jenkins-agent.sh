b59275337dd9163d28fee1a6cfdaebabe033035f
murali <murali.pydi@gmail.com>
2026-01-30 21:43:38 +0530

    code change in files

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -20,25 +20,26 @@ xfs_growfs /
 xfs_growfs /var/tmp
 xfs_growfs /var
 
-yum install java-21-openjdk -y
+sudo yum install java-21-openjdk -y
 # Terraform Installation
-yum install -y yum-utils
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-yum -y install terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
 
 # NodeJs installation
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-yum install zip -y
+sudo dnf module disable nodejs -y
+sudo dnf module enable nodejs:20 -y
+sudo dnf install nodejs -y
+sudo yum install zip -y
 
 # docker
-yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 usermod -aG docker ec2-user
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
--- jenkins.sh
+++ jenkins.sh
@@ -14,9 +14,9 @@ xfs_growfs /var
 
 curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install fontconfig java-21-openjdk -y
-yum install jenkins -y
-systemctl daemon-reload
-systemctl enable jenkins
-systemctl start jenkins
+sudo yum install fontconfig java-21-openjdk -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
 
