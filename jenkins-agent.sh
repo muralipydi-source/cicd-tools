b4e70bf2425cdf7530e9a9e4dcd897c49b63dfbd
murali <murali.pydi@gmail.com>
2026-01-31 16:35:52 +0530

    code change in files

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -1,54 +1,5 @@
-# #!/bin/bash
-
-# #resize disk from 20GB to 50GB
-# #growpart /dev/nvme0n1 4
-
-# #lvextend -L +10G /dev/mapper/RootVG-homeVol
-# #lvextend -L +10G /dev/mapper/RootVG-varVol
-# #lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
-
-# #xfs_growfs /home
-# #xfs_growfs /var/tmp
-# #xfs_growfs /var
-# growpart /dev/nvme0n1 4
-
-# lvextend -L +10G /dev/RootVG/rootVol
-# lvextend -L +10G /dev/mapper/RootVG-varVol
-# lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
-
-# xfs_growfs /
-# xfs_growfs /var/tmp
-# xfs_growfs /var
-
-# sudo yum install java-21-openjdk -y
-# # Terraform Installation
-# sudo yum install -y yum-utils
-# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-# sudo yum -y install terraform
-
-# # NodeJs installation
-# sudo dnf module disable nodejs -y
-# sudo dnf module enable nodejs:20 -y
-# sudo dnf install nodejs -y
-# sudo yum install zip -y
-
-# # docker
-# sudo yum install -y yum-utils
-# sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-# sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-# usermod -aG docker ec2-user
-# sudo systemctl start docker
-# sudo systemctl enable docker
-# sudo systemctl status docker
-
-# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-# chmod 700 get_helm.sh
-# ./get_helm.sh
-
 #!/bin/bash
-#set -e
-
-### Resize disk from 20GB to 50GB ###
+# Resize disk from 20GB to 50GB ###
 sudo growpart /dev/nvme0n1 4
 
 # Resize Physical Volume
@@ -64,21 +15,21 @@ sudo xfs_growfs /
 sudo xfs_growfs /var
 sudo xfs_growfs /var/tmp
 
-### Java Installation ###
+# Java Installation ###
 sudo yum install -y java-21-openjdk
 
-### Terraform Installation ###
+# Terraform Installation ###
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum install -y terraform
 
-### Node.js Installation ###
+# Node.js Installation ###
 sudo dnf module disable nodejs -y
 sudo dnf module enable nodejs:20 -y
 sudo dnf install -y nodejs
 sudo yum install -y zip
 
-### Docker Installation ###
+# Docker Installation ###
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y \
@@ -92,11 +43,15 @@ sudo usermod -aG docker ec2-user
 sudo systemctl enable docker
 sudo systemctl start docker
 
-### Helm Installation ###
+# Helm Installation ###
+# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+# chmod +x get_helm.sh
+# sudo ./get_helm.sh
+
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod +x get_helm.sh
 sudo ./get_helm.sh
-
+helm version
 
 
 # df -h
--- jenkins.sh
+++ jenkins.sh
@@ -1,29 +1,7 @@
-# #!/bin/bash
-
-# #resize disk from 20GB to 50GB
-# growpart /dev/nvme0n1 4
-
-# lvextend -L +10G /dev/RootVG/rootVol
-# lvextend -L +10G /dev/mapper/RootVG-varVol
-# lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
-
-# xfs_growfs /
-# xfs_growfs /var/tmp
-# xfs_growfs /var
-
-
-# curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-# rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-# sudo yum install fontconfig java-21-openjdk -y
-# sudo yum install jenkins -y
-# sudo systemctl daemon-reload
-# sudo systemctl enable jenkins
-# sudo systemctl start jenkins
-
 #!/bin/bash
 #set -e
 
-### Resize disk from 20GB to 50GB ###
+# Resize disk from 20GB to 50GB ###
 sudo growpart /dev/nvme0n1 4
 
 # Resize physical volume so LVM can see new space
